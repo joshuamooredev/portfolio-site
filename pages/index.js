b87462d97ffe189c3e902d403ffbc416c9e920e9
Joshua Moore <mojoshua@sheridancollege.ca>
2025-05-13 18:51:04 -0400

    feat: add title, description, favicon

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,18 @@ import Projects from "@/sections/Projects/Projects";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Joshua | Front-End Portfolio</title>
+        <meta
+          name="description"
+          content="Portfolio of Joshua Moore – a front-end developer crafting sleek, responsive web experiences with precision using React, Next.js, and modern design tools."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
-      <Hero/>
-      <Projects/>
-      <ThemeToggle/>
+      <Hero />
+      <Projects />
+      <ThemeToggle />
     </>
   );
 }
